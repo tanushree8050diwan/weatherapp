36beb047f4f17e92115eb327bab307ce0146ef8b
tanushree8050diwan <149941011+tanushree8050diwan@users.noreply.github.com>
2024-05-19 09:13:29 +0530

    Update app.js

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ function showError(error) {
     notificationElement.innerHTML= `<p> ${error.message} </p>`
 }
 function getSearchWeather(city) {
-    let api= `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
+    let api= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${key}`
 
     fetch(api)
     .then(function (response) {
@@ -73,7 +73,7 @@ function getSearchWeather(city) {
     })
 }
 function getWeather(latitude,logitude) {
-   let api= `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`
+   let api= `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`
 
    fetch(api)
     .then(function (response) {
